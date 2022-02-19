6441617d32d873b0ea4832993b9d94d0f3062c38
jb0gie <up620161@myport.ac.uk>
2022-02-19 03:00:51 +0000

    rah it b like dat

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../libs/theme.js'
+import theme from '../lib/theme.js'
 
 export default class Document extends NextDocument {
   render() {
