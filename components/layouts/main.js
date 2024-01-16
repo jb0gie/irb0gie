6934e375d0ed9751868a0746aa2454e9d59c83d3
j_b0gie <wxzvrd@protonmail.com>
2024-01-16 14:23:34 +0000

    ayyyy i'm on a roll rn

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -14,6 +14,18 @@ const Main = ({ children, router }) => {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9799301594836054"
           crossOrigin="anonymous"
         ></script>
+        <script
+          data-name="BMC-Widget"
+          data-cfasync="false"
+          src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+          data-id="irb0gie"
+          data-description="Support me on Buy me a coffee!"
+          data-message="Thanks for visiting, You can now buy me a coffee (not s̶t̶a̶r̶b̶u̶c̶k̶s̶ please!)"
+          data-color="#FF813F"
+          data-position="Right"
+          data-x_margin="18"
+          data-y_margin="18"
+        ></script>
       </Head>
       <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
